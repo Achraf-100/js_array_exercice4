37af77ceeb29fa35d2bae10ca4696c178f07e634
Achraf <hardy1348@live.fr>
2020-12-30 15:30:08 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+let nouveau = ["hehe", "hoho", "haha", "hzhz"];
+console.log(nouveau);
+
+let name = nouveau.push("Achraf")
+console.log(nouveau);
+
+let news = 0
+nouveau[news] = 'tata'
+console.log(nouveau);
+
+let index = nouveau.indexOf("haha")
+let result = nouveau.splice(index, 1)
+console.log(nouveau);
